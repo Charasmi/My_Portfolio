1b52ea7d6930b86f3e3171380d22eb8e54285827
Charasmi <126413343+Charasmi@users.noreply.github.com>
2025-05-09 22:38:00 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/mine.png"
+        src="/images/mine.png"
         alt="Dhanavath Charasmi"
         className="max-h-[450px] w-auto"
       />
